6704a75fdd3574e02f63c579c2dea0717cf4d81a
Denisszm <stefanszm@yahoo.com>
2025-10-30 18:29:49 +0100

    Många Ändringar och jobb
    Jag har flyttat mina klasser till separata filer inom RoomGenerator scripten, fixat en barebones room generator som ska uppdateras av mig imorgon till att nå bättre krav, jag har gjort en ny empty och fäst scripten till den emptyn.

---
+++ Fix_The_Breaker/Roomgeneration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roomgeneration : MonoBehaviour
+{
+    [SerializeField] private int roomCount = 5;
+    private List<Rooms> rooms = new List<Rooms>();
+    public Random random = new Random();
+
+    void start()
+    {
+        GenerateRooms();
+    }
+    void GenerateRooms()
+    {
+        rooms.Clear();
+        
+        for (int i = 0; i < roomCount; i++)
+        {
+            Vector2Int randomPos = new Vector2Int(random.Range(-20, 20), random.Range(-20, 20));
+            Vector2Int randomSize = new Vector2Int(random.Range(-20, 20), random.Range(-20, 20));
+            Room.Type randomType = (Room.Type)Random.Range(0, System.Enum.GetValues(typeof(Room.Type)).Length)
+            Room newRoom = new Room(randomPos, randomSize, randomType);
+            rooms.Add(newRoom);
+
+            Debug.Log($"Created room #{i + 1}: Type={newRoom.roomType}, Pos={newRoom.position}, Size={newRoom.size}");
+        }
+        Debug.Log($"Generated {rooms.Count} rooms total!");
+    }
+}
---
+++ Fix_The_Breaker/Rooms.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Rooms
+{
+    [SerializeField] public Vector2Int position;
+    public Vector2Int size;
+    public List<Vector2Int> tiles = new List<Vector2Int>();
+
+    public enum Type
+    {
+        Breaker,
+        Breakroom,
+        Office,
+        Corridor,
+        Factory
+    }
+    public Type roomType;
+
+    public Rooms(Vectort2Int pos, Vector2Int sz, Type type)
+    {
+        position = pos;
+        size = sz;
+        roomType = type;
+        tiles = new list<Vector2Int>();
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int y = 0; y < size.y; y++)
+            {
+                tiles.Add(new Vector2Int(position.x + x, position.y + y,));
+            }
+        }
+    }
+}   
