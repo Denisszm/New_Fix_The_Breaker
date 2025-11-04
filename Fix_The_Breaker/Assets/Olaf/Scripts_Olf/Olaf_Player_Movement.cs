33261c8224423666dd3ebf24c277908631954b4d
bamboombox <olafslazynski@gmail.com>
2025-11-04 13:46:58 +0100

    Finally no cancer so picture added

--- Fix_The_Breaker/Assets/Olaf/Scripts_Olf/Olaf_Player_Movement.cs
+++ Fix_The_Breaker/Assets/Olaf/Scripts_Olf/Olaf_Player_Movement.cs
@@ -16,18 +16,18 @@ public class Olaf_Player_Movement : MonoBehaviour
 
         moveDirection = new Vector2(MovementX, MovementY).normalized;
 
-        if (rb.position.y < -2 && rb.position.y > -3)
+        /*if (rb.position.y < -2 && rb.position.y > -3)
         {
             transform.localScale = new Vector3(rb.position.y * 0.7f, rb.position.y * 0.7f, 1);
         }
         else if(rb.position.y >= -2)
         {
-            transform.localScale = new Vector3(-1.5f* 1, -1.5f * 1, 1);
+            transform.localScale = new Vector3(-1.5f* 2, -1.5f * 2, 1);
         }
         else if (rb.position.y <= -3)
         {
             transform.localScale = new Vector3(-2.3f, -2.3f, 1);
-        }
+        }*/
     }
     private void FixedUpdate()
     {
